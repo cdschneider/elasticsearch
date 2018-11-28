ad9fcbd9a294ce5a21ea477e07265f087fd7e699
Christian Schneider <cdschneider@wisc.edu>
2018-11-27 22:40:36 -0600

    starting off

---
+++ buildSrc/src/main/java/org/elasticsearch/gradle/precommit/ForbiddenPatternsTask2.java
@@ -0,0 +1,37 @@
+package org.elasticsearch.gradle.precommit;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.util.PatternFilterable;
+import org.gradle.api.tasks.util.PatternSet;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Checks for patterns in source files for the project which are forbidden.
+ */
+public class ForbiddenPatternsTask2 extends DefaultTask {
+
+    /** The rules: a map from the rule name, to a rule regex pattern. */
+    private static final Map<String, String> patterns = new HashMap<>();
+    static {
+        patterns.put("nocommit", ""); //TODO escape patterns
+        patterns.put("nocommit should be all lowercase or all uppercase", "");
+        patterns.put("tab", "");
+    }
+
+    /** A pattern set of which files should be checked. */
+    private final PatternFilterable filesFilter = new PatternSet()
+        .include("**")
+        .exclude("**/*.gz")
+        .exclude("**/*.ico")
+        .exclude("**/*.jar")
+        .exclude("**/*.zip")
+        .exclude("**/*.jks")
+        .exclude("**/*.crt")
+        .exclude("**/*.png");
+
+    public ForbiddenPatternsTask2() {
+        setDescription("Checks source files for invalid patterns like nocommits or tabs");
+    }
+}
