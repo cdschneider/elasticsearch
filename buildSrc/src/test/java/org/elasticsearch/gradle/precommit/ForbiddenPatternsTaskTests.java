ff425afa8c47c1cc054acaedbb6816a95da394a5
Christian Schneider <cdschneider@wisc.edu>
2018-11-28 20:51:04 -0600

    tests for forbidden pattern task

---
+++ buildSrc/src/test/java/org/elasticsearch/gradle/precommit/ForbiddenPatternsTaskTests.java
@@ -0,0 +1,15 @@
+package org.elasticsearch.gradle.precommit;
+
+import org.elasticsearch.gradle.test.GradleUnitTestCase;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
+
+public class ForbiddenPatternsTaskTests extends GradleUnitTestCase {
+    @Rule
+    public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+    public void testCheckInvalidPatterns() throws Exception {
+        Assert.fail(); //TODO
+    }
+}
