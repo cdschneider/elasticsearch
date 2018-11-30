a5fa88fb1b5ae78932c5bc60d79e3a385840f1b7
Christian Schneider <cdschneider@wisc.edu>
2018-11-30 17:16:56 -0600

    cleaning up imports

--- buildSrc/src/test/java/org/elasticsearch/gradle/precommit/ForbiddenPatternsTaskTests.java
+++ buildSrc/src/test/java/org/elasticsearch/gradle/precommit/ForbiddenPatternsTaskTests.java
@@ -1,20 +1,22 @@
 package org.elasticsearch.gradle.precommit;
 
-import com.carrotsearch.ant.tasks.junit4.dependencies.com.google.common.collect.Maps;
 import org.elasticsearch.gradle.test.GradleUnitTestCase;
 import org.gradle.api.GradleException;
 import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.testfixtures.ProjectBuilder;
 import org.junit.Rule;
-import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 public class ForbiddenPatternsTaskTests extends GradleUnitTestCase {
     @Rule
@@ -83,7 +85,7 @@ public class ForbiddenPatternsTaskTests extends GradleUnitTestCase {
         ForbiddenPatternsTask2 task = createTask(project);
         task.rule(rule);
 
-        List<String> lines = Arrays .asList("GOOD LINE", "//todo", "// some stuff, toDo");
+        List<String> lines = Arrays.asList("GOOD LINE", "//todo", "// some stuff, toDo");
         File file = new File(project.getProjectDir(), "src/main/java/Moot.java");
         file.getParentFile().mkdirs();
         file.createNewFile();
